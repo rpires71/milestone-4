ead14d521928954a16e2b2e092a74d84f3d99e18
Roberto Pires <roberto.pires@gmail.com>
2026-06-15 22:32:20 +0100

    feat: add shop listing view, url and template

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import ProductCategory, Product
 
-admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'is_available')
+    list_filter = ('category', 'is_available')
+    search_fields = ('name', 'brand', 'description')
+    prepopulated_fields = {'slug': ('name',)}
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """Display all available products."""
+    products = Product.objects.filter(is_available=True)
+    context = {
+        'products': products,
+    }
+    return render(request, 'shop/products.html', context)
