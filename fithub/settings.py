272eead5de48de7c07dc6a843352bcf527a7b0d7
Roberto Pires <roberto.pires@gmail.com>
2026-06-21 12:39:54 +0100

    feat: configure Gmail SMTP for production email

--- fithub/settings.py
+++ fithub/settings.py
@@ -128,7 +128,17 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Console email backend for development (logs emails to terminal)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if "DEVELOPMENT" in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'fithub@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # allauth account settings (current syntax)
 ACCOUNT_LOGIN_METHODS = {'username', 'email'}
