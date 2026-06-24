931380e9ac6f4627d1ba6aae31510d8ef9fb6ed0
Roberto Pires <roberto.pires@gmail.com>
2026-06-24 23:10:04 +0100

    feat: add featured plans and products to home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from plans.models import Plan
+from shop.models import Product
 
 
 def index(request):
-    """Render the FitHub homepage."""
-    return render(request, 'home/index.html')
+    """Render the FitHub homepage with featured plans and products."""
+    featured_plans = Plan.objects.filter(status='published')[:3]
+    featured_products = Product.objects.filter(is_available=True)[:4]
+    context = {
+        'featured_plans': featured_plans,
+        'featured_products': featured_products,
+    }
+    return render(request, 'home/index.html', context)
